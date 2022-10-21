5df52542672cc17b617b4cca337a1e05c3544d52
dai <d.a.i-ichiro@outlook.jp>
2022-10-21 17:19:26 +0900

    add fonttest2.py

--- collectfonts/collectfonts.py
+++ collectfonts/collectfonts.py
@@ -1,8 +1,10 @@
 import os
 import glob
 import shutil
+from torchvision.datasets.utils import download_url
 
-font_index = [135, 136, 137]
+# Copy from Windows 11
+font_index = [84, 85, 179, 180, 181]
 fonts = glob.glob('c://Windows/Fonts/*.ttf')
 
 os.makedirs('fonts', exist_ok=True)
@@ -12,4 +14,12 @@ for i in font_index:
     fname = fname.replace('.TTF', '.ttf')
     shutil.copy(fonts[i], os.path.join('fonts', fname))
     
-    
+# Download from mmocr
+font_url = 'https://download.openmmlab.com/mmocr/data/font.TTF'
+font_fname = 'mmocr.ttf'
+download_url(font_url, root = 'fonts', filename = font_fname)
+
+# Download from TextRecognitionDataGenerator
+font_url = 'https://raw.githubusercontent.com/Belval/TextRecognitionDataGenerator/master/trdg/fonts/ja/TakaoMincho.ttf'
+font_fname = font_url.split('/')[-1]
+download_url(font_url, root = 'fonts', filename = font_fname)
---
+++ collectfonts/fonttest2.py
@@ -0,0 +1,24 @@
+import os
+import glob
+from PIL import Image, ImageDraw, ImageFont
+
+os.makedirs('testresults', exist_ok=True)
+
+fonts = glob.glob('fonts/*.ttf')
+
+text = '「アート」\n【あーと】\n東京駅'
+
+for i, _font in enumerate(fonts):
+
+    im = Image.new("RGB", (256, 256), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+
+    font = ImageFont.truetype(_font, 48)
+
+    x = 20
+    y = 20
+
+    draw.multiline_text((x, y), text, fill=(0, 0, 255), font=font)
+        
+    im.save(os.path.join('testresults', f'{i}.jpg'))
+
