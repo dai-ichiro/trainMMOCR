bc8de533c100d87b5013da3020aad394a10f99a3
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2022-10-21 16:18:33 +0900

    Update convert.py

--- makedataset/convert.py
+++ makedataset/convert.py
@@ -22,11 +22,8 @@ train_num = int(num * 0.9)
 train_list = random.sample(label_list, train_num)
 test_set = set(label_list) - set(train_list)
 
-
 with open('train_label.txt', 'w', encoding='utf-8') as f:
     f.write('\n'.join(train_list))
 
 with open('test_label.txt', 'w', encoding='utf-8') as f:
     f.write('\n'.join(test_set))
-
-
