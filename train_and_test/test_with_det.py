fe1e90835939f9f85cd0d6766e93c62cd4a4d2b0
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2022-10-22 09:46:21 +0900

    Update test_with_det.py

--- train_and_test/test_with_det.py
+++ train_and_test/test_with_det.py
@@ -6,7 +6,7 @@ cfg = 'japanese_cfg.py'
 img = 'sampleimage/test1.png'
 checkpoint = 'output/latest.pth'
 
-# Detection: PANet_IC15
+# Detection: fcenet
 det_checkpoint_name = 'fcenet_r50dcnv2_fpn_1500e_ctw1500'
 os.makedirs('models', exist_ok=True)
 det_checkpoint = download(package='mmocr', configs=[det_checkpoint_name], dest_root="models")[0]
