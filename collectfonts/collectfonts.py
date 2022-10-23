3ed9799899cbf57f26b8ea194bd30dafa3a6adbe
dai <d.a.i-ichiro@outlook.jp>
2022-10-24 08:47:11 +0900

    modify collectfonts.py

--- collectfonts/collectfonts.py
+++ collectfonts/collectfonts.py
@@ -3,17 +3,17 @@ import glob
 import shutil
 from torchvision.datasets.utils import download_url
 
+os.makedirs('fonts', exist_ok=True)
+
 # Copy from Windows 11
 font_index = [84, 85, 179, 180, 181]
 fonts = glob.glob('c://Windows/Fonts/*.ttf')
-
-os.makedirs('fonts', exist_ok=True)
 
 for i in font_index:
     fname = os.path.basename(fonts[i])
     fname = fname.replace('.TTF', '.ttf')
     shutil.copy(fonts[i], os.path.join('fonts', fname))
-    
+   
 # Download from mmocr
 font_url = 'https://download.openmmlab.com/mmocr/data/font.TTF'
 font_fname = 'mmocr.ttf'
